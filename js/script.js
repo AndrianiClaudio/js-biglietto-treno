13b3b371a825176c4e50b642229f8af57af590b4
AndrianiClaudio <claudioandriani1994@gmail.com>
2021-11-19 15:42:44 +0100

    calcolo prezzi con sconti per eta

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 // Richiesta numero di chilometri e età
-let km = prompt('Inserire il numero di chilometri');
-console.log(km);
-let age = prompt("Inserire l'età");
-console.log(age);
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+const km = parseInt(prompt('Inserire il numero di chilometri'));
+// console.log(km);
+const age = parseInt(prompt("Inserire l'età"));
+// console.log(age);
 // il prezzo del biglietto è definito in base ai km(0.21 € al km)
-// va applicato uno sconto del 20 % per i minorenni
-// va applicato uno sconto del 40 % per gli over 65.
+const priceForKm = 0.21;
+let price = km * priceForKm;
+// console.log(price);
+// va applicato uno sconto del 20 % per i minorenni e uno sconto del 40 % per gli over 65.
+const minorSale = 0.20; //1 = 100%
+const over65sale = 0.40;
+if (age < 18) { // Minorenni
+    price *= (1 - minorSale);
+    console.log(price);
+}
+else if (age > 65) { //Over 65
+    price *= (1 - over65sale);
+    console.log(price);
+}
 // L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).Questo richiederà un minimo di ricerca.
