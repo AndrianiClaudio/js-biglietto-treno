393a4931825c1cfbb05cccad95957ca008bdc402
AndrianiClaudio <claudioandriani1994@gmail.com>
2021-11-19 16:33:42 +0100

    completamento(senza controllo errori inserimento)

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
 // Richiesta numero di chilometri e età
 const km = parseInt(prompt('Inserire il numero di chilometri'));
-// console.log(km);
+console.log(km);
 const age = parseInt(prompt("Inserire l'età"));
-// console.log(age);
+console.log(age);
 // il prezzo del biglietto è definito in base ai km(0.21 € al km)
 const priceForKm = 0.21;
-let price = km * priceForKm;
-// console.log(price);
+const basePrice = km * priceForKm;
+console.log(basePrice);
 // va applicato uno sconto del 20 % per i minorenni e uno sconto del 40 % per gli over 65.
 const minorSale = 0.20; //1 = 100%
 const over65sale = 0.40;
+let saleValue; //variabile per salvare lo sconto
 if (age < 18) { // Minorenni
-    price *= (1 - minorSale);
-    price = parseFloat(price).toFixed(2)
-    // console.log(price);
+    saleValue = basePrice * minorSale;
 }
 else if (age > 65) { //Over 65
-    price *= (1 - over65sale);
-    // console.log(price);
+    saleValue = basePrice * over65sale;
 }
-// L’output del prezzo finale va messo fuori in forma umana(con massimo due decimali, per indicare centesimi sul prezzo).Questo richiederà un minimo di ricerca.
+console.log(saleValue);
+// calcolo prezzo con arrotondamento a due cifre
+const salePrice = parseFloat(basePrice - saleValue);
+console.log(salePrice);
+// Stampa dettagli utente e prezzo.
 document.getElementById('ticket-price').innerHTML = `
 <span>Numero km: </span>${km}<br>
 <span>Anni: </span>${age}<br>
-<p>Sconti: 20% per i minorenni - 40% per gli over 65</p>
-<strong>Prezzo complessivo: </strong>${price}
+================================================
+<p style = "padding: 0;margin: 0;">Sconti: 20% per i minorenni - 40% per gli over 65</p>
+================================================<br>
+<span>Prezzo base: </span>${basePrice}
+<span>Valore dello sconto: </span>${saleValue}
+<strong>Prezzo complessivo: </strong>${salePrice}
 `;
